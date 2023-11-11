aa67b05689dfc55d1896483ef2e7120903ec9a5f
Niestrat99 <niestrat99@gmail.com>
2023-11-11 18:07:07 +0100

    feat: adding various potion effects for traps

--- src/main/java/io/github/thatsmusic99/hitwshitpost/listeners/PlayerListener.java
+++ src/main/java/io/github/thatsmusic99/hitwshitpost/listeners/PlayerListener.java
@@ -12,13 +12,59 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class PlayerListener implements Listener {
+    int sec = 20;
     PotionEffect jumpBoost = new PotionEffect(PotionEffectType.JUMP, PotionEffect.INFINITE_DURATION, 1, false, false);
+    PotionEffect springy = new PotionEffect(PotionEffectType.JUMP, (sec * 8), 3, false, false);
+    PotionEffect speedBoost = new PotionEffect(PotionEffectType.SPEED, (sec * 8), 4, false, false);
+    PotionEffect legDay = new PotionEffect(PotionEffectType.SLOW, (sec * 10), 2, false, false);
+
+    PotionEffect gravity = new PotionEffect(PotionEffectType.SLOW_FALLING, (sec * 15), 1, false, false);
+
+    // Speedboost Trap
+    public void applySpeed() {
+        for (Player p : Bukkit.getOnlinePlayers())  {
+            p.addPotionEffect(speedBoost);
+        }
+    }
+
+    public void applySlow() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.addPotionEffect(legDay);
+        }
+    }
+
+    public void applySpringy() {
+        for (Player p : Bukkit.getOnlinePlayers())  {
+            p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(springy);
+
+            Bukkit.getScheduler().runTaskLater(HITWShitpost.get(), new Runnable() {
+                @Override
+                public void run() {
+                    p.addPotionEffect(jumpBoost);
+                }
+            }, 400L);
+        }
+    }
+
+    public void applyGravity() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.addPotionEffect(gravity);
+        }
+    }
 
     // Join Event
     @EventHandler
     public void playerJoinEvent(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         jumpBoost.apply(player);
+
+        Bukkit.getScheduler().runTaskLater(HITWShitpost.get(), new Runnable() {
+            @Override
+            public void run() {
+                applyGravity();
+            }
+        }, 100L);
     }
     // Quit Event - making sure to remove the jump boost from the player.
     @EventHandler
@@ -38,7 +84,7 @@ public class PlayerListener implements Listener {
                     public void run() {
                         jumpBoost.apply(player);
                     }
-                    }, 10L);
+                    }, 5L);
             }
         }
     }
