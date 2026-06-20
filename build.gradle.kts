0daf51f8b0e8820609ddb6bffd0c84befb4740f8
Niestrat99 <niestrat99@gmail.com>
2026-06-20 10:42:12 +0200

    chore: Update plugin JDK and Gradle version for 26.1.2+ compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("xyz.jpenilla.run-paper") version "2.2.0"
+    //id("xyz.jpenilla.run-paper") version "2.2.0"
 }
 
 group = "io.github.thatsmusic99"
@@ -8,15 +8,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-
-    maven("https://repo.papermc.io/repository/maven-public/")
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
-    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
 }
 
 tasks.getByName<Test>("test") {
@@ -24,11 +26,11 @@ tasks.getByName<Test>("test") {
 }
 
 tasks {
-    runServer {
-        minecraftVersion("1.20.2")
-    }
+ /*   runServer {
+        minecraftVersion("26.1.2")
+    }*/
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
